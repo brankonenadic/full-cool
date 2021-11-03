e0e3b0ea97cea4ac5a5e11369b2e67bb5e5f4ea4
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-03 22:30:05 +0100

    Update

--- src/components/sections/Navbar.js
+++ src/components/sections/Navbar.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import classes from './Navbar.module.css';
-
+import { Link as LinkR } from 'react-router-dom';
+import { Link as LinkS } from 'react-scroll';
 const Navbar = () => {
     return (
         <div className={classes.nav}>
             <div className={classes.navConteiner}>
-                <div className={classes.navLogo}>
-LaninTata
-                </div>
+                <LinkR to="/home" className={classes.navLogo}>LaninTata</LinkR>
+
+                <div className={classes.menuIcon}><i className="fas fa-bars"></i></div>
+                <ul className={classes.navList}>
+                    <li className={classes.navListItem}><LinkS className={classes.navLinkS} to="header">Link1</LinkS></li>
+                    <li className={classes.navListItem}><LinkS className={classes.navLinkS} to="header">Link2</LinkS></li>
+                    <li className={classes.navListItem}><LinkS className={classes.navLinkS} to="header">Link3</LinkS></li>
+                </ul>
             </div>
         </div>
     )
