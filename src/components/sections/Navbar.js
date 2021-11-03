ef48ab2aee4bd4a6421b8640f743e556ac4553ea
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-03 21:12:05 +0100

    Update

---
+++ src/components/sections/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Navbar.module.css';
+
+const Navbar = () => {
+    return (
+        <div className={classes.nav}>
+            
+        </div>
+    )
+}
+
+export default Navbar
