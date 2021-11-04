67c4b73103e3c662e3e5ecaeb3449737b3c42366
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-04 15:28:03 +0100

    Update

--- src/components/sections/Navbar.js
+++ src/components/sections/Navbar.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Navbar.module.css';
 import { Link as LinkR } from 'react-router-dom';
 import { Link as LinkS } from 'react-scroll';
+
+
 const Navbar = () => {
+    const [openMenu, setopenMenu] = useState(false);
+
+    const openMenuHandler = () => {
+        setopenMenu(true);
+    }
+    const closeMenuHandler = () => {
+        setopenMenu(false);
+    }
+    let iconMenu = !openMenu ? <i onClick={openMenuHandler} className="fas fa-bars"></i> : <i onClick={closeMenuHandler} className="fas fa-times"></i>;
     return (
         <div className={classes.nav}>
             <div className={classes.navConteiner}>
                 <LinkR to="/home" className={classes.navLogo}>LaninTata</LinkR>
 
-                <div className={classes.menuIcon}><i className="fas fa-bars"></i></div>
+                <div className={classes.menuIcon}>{iconMenu}</div>
                 <ul className={classes.navList}>
                     <li className={classes.navListItem}><LinkS className={classes.navLinkS} to="header">Link1</LinkS></li>
                     <li className={classes.navListItem}><LinkS className={classes.navLinkS} to="header">Link2</LinkS></li>
                     <li className={classes.navListItem}><LinkS className={classes.navLinkS} to="header">Link3</LinkS></li>
                 </ul>
                 <div className={classes.navBtn}>
-              <LinkR to="/sing-up"  className={classes.navBtnLink}>Sing up</LinkR>
+                    <LinkR to="/sing-up" className={classes.navBtnLink}>Sing up</LinkR>
                 </div>
             </div>
         </div>
