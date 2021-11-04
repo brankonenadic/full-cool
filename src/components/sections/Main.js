ac2638330e250fcc821085e00321bd53611390dc
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-04 22:09:11 +0100

    Update

--- src/components/sections/Main.js
+++ src/components/sections/Main.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from 'react';
+import classes from './Main.module.css';
 
 const Main = () => {
     return (
-        <div id="main">
+        <div id="main" className={classes.main}>
             <h2>Some Subtitle</h2>
             <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab illo voluptatibus reprehenderit corrupti eius libero temporibus blanditiis ipsum quam asperiores, voluptates vitae numquam architecto labore minus maiores corporis culpa ratione. 
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab illo voluptatibus reprehenderit corrupti eius libero temporibus blanditiis ipsum quam asperiores, voluptates vitae numquam architecto labore minus maiores corporis culpa ratione.
             </p>
-            
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab illo voluptatibus reprehenderit corrupti eius libero temporibus blanditiis ipsum quam asperiores, voluptates vitae numquam architecto labore minus maiores corporis culpa ratione.
+            </p>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab illo voluptatibus reprehenderit corrupti eius libero temporibus blanditiis ipsum quam asperiores, voluptates vitae numquam architecto labore minus maiores corporis culpa ratione.
+            </p>
         </div>
     )
 }
