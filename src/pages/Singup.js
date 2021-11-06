196fcb7b7a45d4eea0d1895e464d1153d38fca5f
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-06 22:24:34 +0100

    Update

--- src/pages/Singup.js
+++ src/pages/Singup.js
@@ -13,7 +13,7 @@ const Singup = () => {
                 <input className={classes.singupinput} name="email" placeholder="Enter email..." type="email" required />
                 <label className={classes.singuplabel} htmlFor="password">Password</label>
                 <input className={classes.singupinput} name="password" autoComplete="false" placeholder="Enter password..." type="password" required />
-                <label className={classes.singuplabel} htmlFor="re-password">Re.password</label>
+                <label className={classes.singuplabel} htmlFor="re-password">Re-password</label>
                 <input className={classes.singupinput} name="re-password" autoComplete="false" placeholder="Retype password..." type="password" required />
                 <button className={classes.singupbutton}> Sing up</button>
             </form>
