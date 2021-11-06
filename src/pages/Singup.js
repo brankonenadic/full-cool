735d35121d1b50fc0a06a4e74f742b78083b62c6
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-06 19:03:41 +0100

    Update

--- src/pages/Singup.js
+++ src/pages/Singup.js
@@ -5,17 +5,17 @@ import { Link as LinkR } from 'react-router-dom';
 const Singup = () => {
     return (
         <div className={classes.wrapper}>
-           <LinkR to="/home"  className={classes.back}> <i className="fas fa-chevron-left"></i> LaninTata</LinkR>
-            <form id="singup-form" >
-                <label htmlFor="name">Full Name</label>
-                <input name="name" placeholder="Enter full name..." type="text" required />
-                <label htmlFor="email">Email</label>
-                <input name="email" placeholder="Enter email..." type="email" required />
-                <label htmlFor="password">Password</label>
-                <input name="password" autoComplete="false" placeholder="Enter password..." type="password" required />
-                <label htmlFor="re-password">Re.password</label>
-                <input name="re-password" autoComplete="false" placeholder="Retype password..." type="password" required />
-                <button> Sing up</button>
+            <LinkR to="/home" className={classes.back}> <i className="fas fa-chevron-left"></i> LaninTata</LinkR>
+            <form id="singup-form" className={classes.singup}>
+                <label className={classes.singuplabel} htmlFor="name">Full Name</label>
+                <input className={classes.singupinput} name="name" placeholder="Enter full name..." type="text" required />
+                <label className={classes.singuplabel} htmlFor="email">Email</label>
+                <input className={classes.singupinput} name="email" placeholder="Enter email..." type="email" required />
+                <label className={classes.singuplabel} htmlFor="password">Password</label>
+                <input className={classes.singupinput} name="password" autoComplete="false" placeholder="Enter password..." type="password" required />
+                <label className={classes.singuplabel} htmlFor="re-password">Re.password</label>
+                <input className={classes.singupinput} name="re-password" autoComplete="false" placeholder="Retype password..." type="password" required />
+                <button className={classes.singupbutton}> Sing up</button>
             </form>
         </div>
     )
