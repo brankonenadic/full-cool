250df2fee93080f0a6af35e66deb2562dd61287d
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-07 22:19:03 +0100

    Update

--- src/components/ui/Modal.js
+++ src/components/ui/Modal.js
@@ -1,23 +1,26 @@
 import React from 'react';
+import { Fragment } from 'react/cjs/react.production.min';
 import classes from './Modal.module.css';
-
+import reactDom from 'react-dom';
 
 const Backdrop = (props) => {
     return (
-<div className={classes.backdrop}></div>
+        <div className={classes.backdrop}></div>
     );
 }
 const Modaloverlay = (props) => {
     return (
-<div className={classes.modal}></div>
+        <div className={classes.modal}></div>
     );
 }
+const portalElement = document.getElementById('overlays');
 const Modal = (props) => {
     return (
-        <div>
-            
-        </div>
-    )
+        <Fragment>
+            {ReactDOM.createPortal(<Backdrop />), portalElement}
+            {ReactDOM.createPortal(<Modaloverlay>{props.children}</Modaloverlay>), portalElement}
+        </Fragment>
+    );
 }
 
 export default Modal
---
+++ src/layout/SingupForm.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import classes from './SingupForm.module.css';
+import { Link as LinkR } from 'react-router-dom';
+import Modal from '../ui/Modal';
+
+const Singup = () => {
+    return (
+        <Modal>
+            <LinkR to="/home" className={classes.back}> <i className="fas fa-chevron-left"></i> LaninTata</LinkR>
+            <form id="singup-form" className={classes.singup}>
+                <label className={classes.singuplabel} htmlFor="name">Full Name</label>
+                <input className={classes.singupinput} name="name" placeholder="Enter full name..." type="text" required />
+                <label className={classes.singuplabel} htmlFor="email">Email</label>
+                <input className={classes.singupinput} name="email" placeholder="Enter email..." type="email" required />
+                <label className={classes.singuplabel} htmlFor="password">Password</label>
+                <input className={classes.singupinput} name="password" autoComplete="false" placeholder="Enter password..." type="password" required />
+                <label className={classes.singuplabel} htmlFor="re-password">Re-password</label>
+                <input className={classes.singupinput} name="re-password" autoComplete="false" placeholder="Retype password..." type="password" required />
+                <button className={classes.singupbutton}> Sing up</button>
+            </form>
+            </Modal>
+    )
+}
+
+export default Singup
