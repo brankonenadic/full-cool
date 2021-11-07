b113e744a6036eb3fa53f75a722ded7e04ba0c85
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-07 23:13:16 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import { Switch, Route, Redirect } from "react-router-dom";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Home from "./pages/Home";
-import Singup from "./pages/Singup";
+import SingupForm from "./pages/Singup";
 
 
 function App() {
+  const [issignup, setIssignup] = useState(false);
   return (
     <Fragment>
+      {/* <SingupForm /> */}
       <Switch>
         <Route path="/" exact>
           <Redirect to="/home" />
         </Route>
         <Route path="/home" exact>
-        <Home />
-        </Route>
-        <Route path="/sing-up" exact>
-          <Singup />
+          <Home />
         </Route>
       </Switch>
     </Fragment>
--- src/components/ui/Modal.js
+++ src/components/ui/Modal.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import { Fragment } from 'react/cjs/react.production.min';
+import React, { Fragment } from 'react';
 import classes from './Modal.module.css';
-import reactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
 const Backdrop = (props) => {
+    return (<div className={classes.backdrop} />);
+};
+
+const ModalOverlay = (props) => {
     return (
-        <div className={classes.backdrop}></div>
-    );
-}
-const Modaloverlay = (props) => {
-    return (
-        <div className={classes.modal}></div>
+        <div className={classes.modal}>
+            <div className={classes.content}>{props.children}</div>
+        </div>
     );
 }
 const portalElement = document.getElementById('overlays');
 const Modal = (props) => {
     return (
         <Fragment>
-            {ReactDOM.createPortal(<Backdrop />), portalElement}
-            {ReactDOM.createPortal(<Modaloverlay>{props.children}</Modaloverlay>), portalElement}
+            {ReactDOM.createPortal(<Backdrop />, portalElement)}
+            {ReactDOM.createPortal(
+                <ModalOverlay>{props.children}</ModalOverlay>,
+                portalElement
+            )}
         </Fragment>
     );
 }
 
 export default Modal
+
+
+
+
