16d9cbcf2a77908b03e41baff98beab774f1017e
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-10 19:48:10 +0100

    Update

--- src/layout/SinginForm.js
+++ src/layout/SinginForm.js
@@ -6,14 +6,14 @@ const SinginForm = () => {
     return (
         <Modal>
             <div>
-                <form id="singin-form" className={classes.singup}>
-                    <label className={classes.singuplabel} htmlFor="email">Email</label>
-                    <input className={classes.singupinput} name="email" placeholder="Enter email..." type="email" required />
-                    <label className={classes.singuplabel} htmlFor="password">Password</label>
-                    <input className={classes.singupinput} name="password" autoComplete="false" placeholder="Enter password..." type="password" required />
-                    <button className={classes.singupbutton}> Sing in</button>
+                <form id="singin-form">
+                    <label htmlFor="email">Email</label>
+                    <input name="email" placeholder="Enter email..." type="email" required />
+                    <label htmlFor="password">Password</label>
+                    <input name="password" autoComplete="false" placeholder="Enter password..." type="password" required />
+                    <button> Sing in</button>
                 </form>
-                <button className={classes.close} onClick={props.onClose}>Close</button>
+                <button>Close</button>
             </div>
         </Modal>
     )
